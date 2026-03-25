9407b759ac6b430e0e7e6c6318ea0a1e83429994
KaleeswaranGreens <kaleswaranomrbranch@gmail.com>
2026-03-25 18:46:51 +0530

    Modified Made in All

--- Employee.java
+++ Employee.java
@@ -1,5 +1,6 @@
 public class Employee{
     public void empName(){
         System.out.println("Employ Name is : Ram");
+        System.out.println("Employee Was Updated");
     }
 }
--- Sample.js
+++ Sample.js
@@ -1,1 +1,2 @@
 console.log("Day 2 GIT")
+console.log("Updation was Made in local");
