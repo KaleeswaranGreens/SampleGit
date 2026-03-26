abc7eafe64eb41c1f78a5ce0c9649c5670467144
KaleeswaranGreens <kaleswaranomrbranch@gmail.com>
2026-03-26 18:15:53 +0530

    Employee Updated

--- Employee.java
+++ Employee.java
@@ -7,4 +7,10 @@ public class Employee{
     public void empId(){
         System.out.println("Employee Id is : 209");
     }
+
+    public static void main(String[] args) {
+        Employee e = new Employee();
+        e.empId();
+        e.empName();
+    }
 }
